db48e5c5e0a1bd326d34c8c662c46eaf90109d80
VigneshNayakQA <vigneshnayakudupi@gmail.com>
2026-03-27 00:34:00 -0800

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,1 +1,32 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    // Locators
+    By username = By.id("username");
+    By password = By.id("password");
+    By loginButton = By.id("login");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void enterUsername(String user) {
+        driver.findElement(username).sendKeys(user);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(password).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+}
